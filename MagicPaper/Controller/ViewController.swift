e7b3aa8ef2b4e6c69735e1871a2f20c59972c8a9
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-21 16:57:43 -0700

    Add Image Detection in AR

---
+++ MagicPaper/Controller/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  MagicPaper
+//
+//  Created by Philip Yu on 7/21/19.
+//  Copyright © 2019 Philip Yu. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    // Outlets
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewsPaperImages", bundle: .main) {
+            configuration.trackingImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+        
+    }
+    
+    //MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
+        }
+        
+        return node
+        
+    }
+    
+}
